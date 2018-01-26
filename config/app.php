25addb8cba371548c09a81833dace49db96bcd74
vagrant <vagrant@homestead>
2018-01-26 07:51:18 +0000

    pulldown web

--- config/app.php
+++ config/app.php
@@ -234,6 +234,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'CsvReader' => 'app\Library\CsvReader',
+        'Input' => Illuminate\Support\Facades\Input::class,
 
     ],
 
--- resources/views/rikunavis/index.blade.php
+++ resources/views/rikunavis/index.blade.php
@@ -2,13 +2,72 @@
 
 @section('content')
 
-    @foreach($data as $item)
-        <div>
-            <div>{{{ $item->company_name }}}</div>
-            <div>{{{ $item->ceo }}}</div>
-            <div>{{{ $item->sales }}}</div>
-        </div>
-        <hr>
-    @endforeach
+    <form method="POST" action="/contact">
+        <dl>
+            <dt>業種(まとめ)：</dt>
+            <dd>
+                <select name="largeIndustries">
+                    @foreach($datas as $data)
+                        <option value = "{{$data->large_industries_id}}">{{$data->large_industries_name}}</option>
+                    @endforeach
+                </select>
+            </dd>
+        </dl>
+        <dl>
+            <dt>詳細業種：</dt>
+            <dd>
+                <select name="smallIndustries">
+                    @foreach($datas2 as $data)
+                        <option value = "{{$data->small_industries_id}}">{{$data->small_industries_name}}</option>
+                    @endforeach
+                </select>
+            </dd>
+        </dl>
+        <dl>
+            <dt>職種(まとめ)：</dt>
+            <dd>
+                <select name="largeJob">
+                    @foreach($datas3 as $data)
+                        <option value = "{{$data->large_job_category_id}}">{{$data->large_job_category_name}}</option>
+                    @endforeach
+                </select>
+            </dd>
+        </dl>
+        <dl>
+            <dt>詳細職種：</dt>
+            <dd>
+                <select name="smallJob">
+                    @foreach($datas4 as $data)
+                        <option value = "{{$data->small_job_category_id}}">{{$data->small_job_category_name}}</option>
+                    @endforeach
+                </select>
+            </dd>
+        </dl>
+        <dl>
+            <dt>地域：</dt>
+            <dd>
+                <select name="region">
+                    @foreach($datas5 as $data)
+                        <option value = "{{$data->region_id}}">{{$data->region_name}}</option>
+                    @endforeach
+                </select>
+            </dd>
+        </dl>
+        <dl>
+            <dt>都道府県：</dt>
+            <dd>
+                <select name="prefecture">
+                    @foreach($datas6 as $data)
+                        <option value = "{{$data->prefecture_id}}">{{$data->prefecture_name}}</option>
+                    @endforeach
+                </select>
+            </dd>
+        </dl>
+        <input type="submit" value="CSV出力">
+        <input type="hidden"  name="_token" value="{{ csrf_token() }}">
+    </form>
+
+
+
 
 @endsection
